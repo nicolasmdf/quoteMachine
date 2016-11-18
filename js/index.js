72cbb43fcc6e9f2019a5ba96363691d704a7972f
Nicolas Reboredo <nicolasmdf@gmail.com>
2016-11-18 17:35:52 -0300

    fixing update

--- js/index.js
+++ js/index.js
@@ -3,14 +3,12 @@
       $("#textBox").fadeTo(300, 0);
       $.getJSON("http://api.icndb.com/jokes/random", function(json){
         $("#textBox").html("'" + json.value.joke + "'" + "<br> -Anonymous.").fadeTo(1000, 1);
-        $("#textBox").fadeOut("slow");  
-      
-      $.getJSON("http://api.icndb.com/jokes/random", function(json){
-        $("#textBox").html("'" + json.value.joke + "'" + "<br> -Anonymous.").fadeIn("slow");
-          var link =  "https://twitter.com/intent/tweet?text=" + encodeURIComponent(json.value.joke);
+        
+        var link =  "https://twitter.com/intent/tweet?text=" + encodeURIComponent(json.value.joke);
+        
         $(".btn-info").on("click", function(){
           location.href = link;
         });
       });
     });
-})
+});
